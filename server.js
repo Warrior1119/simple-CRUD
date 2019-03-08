bebac679c1e3baf08242660d57cb3f74c652a930
Miroljub <miroljub902@gmail.com>
2019-03-08 18:38:56 +0100

    crud complete

--- server.js
+++ server.js
@@ -18,18 +18,17 @@ app.get('/api/courses', function(req, res) {
 });
 
 app.get('/api/courses/:id', function(req, res) {
-  var course = courses.find(c => c.id === req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course) {
-    res.status(404).send('This course with the given ID was not found');
+    return res.status(404).send('This course with the given ID was not found');
   }
   res.send(course);
 });
 
 app.post('/api/courses', function(req, res) {
-  if (!req.body.name || req.body.name.length < 3) {
-    // 400 Bad Request
-    res.status(400).send('Name is required and should be minimum 3 characters.'); 
-    return;
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
   const course = {
     id: courses.length + 1,
@@ -38,6 +37,39 @@ app.post('/api/courses', function(req, res) {
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', function(req, res) {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('This course with the given ID was not found');
+  }
+
+  const { error } = validateCourse(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message); 
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', function(req, res) {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send('This course with the given ID was not found');
+  }
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  return Joi.validate(course, schema); 
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
